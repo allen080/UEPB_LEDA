f411fb3bac80ab8fe4c4844192fd14ced906da0a
luan fellipe <luanfelliipe8@gmail.com>
2021-05-08 16:59:06 -0300

    Add files via upload

--- Projeto_Covid19/CovidDataControl.py
+++ Projeto_Covid19/CovidDataControl.py
@@ -17,10 +17,10 @@ else:
 	trash = "/dev/null"
 	clear = "clear"
 
-r = system(f"javac -version 2> {trash} > {trash} && java -version 2> {trash} > {trash}");	
+r = system("javac -version 2> {0} > {0} && java -version 2> {0} > {0}".format(trash));	
 
 if r!=0:
-	system(f"echo [!] java nao encontrado. Verifique se o Java esta instalado corretamente no seu sistema.");
+	system("echo [!] java nao encontrado. Verifique se o Java esta instalado corretamente no seu sistema.");
 	input("\nPressione qualquer tecla para finalizar...")
 	exit(1)
 
@@ -29,6 +29,6 @@ system("echo [*] Compilando...");
 if not path.isdir("bin"):
 	mkdir("bin")
 
-system(f"javac -d bin src/*.java && cd bin && {clear} && java Main");
+system("javac -d bin src/*.java && cd bin && {} && java Main".format(clear));
 
 input("\nPressione qualquer tecla para finalizar...")
