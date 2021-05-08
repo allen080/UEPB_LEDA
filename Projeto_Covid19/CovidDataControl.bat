8c918c42e9a3d614fad1f421cda538db5775ccd5
luan fellipe <luanfelliipe8@gmail.com>
2021-05-08 17:22:19 -0300

    Add files via upload

--- Projeto_Covid19/CovidDataControl.bat
+++ Projeto_Covid19/CovidDataControl.bat
@@ -1,7 +1,14 @@
-import os
+@echo off
+cls
 
-os.system("echo [*] Compilando...");
-os.system("if not exist bin md bin");
-os.system("javac -d bin src/*.java && cd bin && cls && java Main");
-
-os.system("pause");
+javac -version 2> nul > nul && java -version 2> nul > nul && (
+	color f
+	echo [*] Compilando...
+	if not exist bin md bin
+	javac -d bin src/*.java && cd bin && cls && java Main
+) || (
+	color c
+	echo [!] java nao encontrado. Verifique se o Java esta instalado corretamente no seu sistema.
+)
+echo.
+pause
