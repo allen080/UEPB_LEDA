260f039d14f96e8173efa4ece82c278274a2b915
luan fellipe <luanfelliipe8@gmail.com>
2021-05-08 15:28:29 -0300

    Add files via upload

--- Projeto_Covid19/CovidDataControl.sh
+++ Projeto_Covid19/CovidDataControl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-function pauseProg(){ echo -e "Pressione qualquer tecla para finalizar...\n" && read; }
+function pauseProg(){ echo -e "\nPressione qualquer tecla para finalizar...\n" && read; }
 
-javac -version 2> /dev/null > /dev/null && java -version 2> nul > nul
+javac -version 2> /dev/null > /dev/null && java -version 2> /dev/null > /dev/null
 
 if [ $? -ne 0 ]; then
-	echo [!] java.exe nao encontrado. Verifique se o Java esta instalado corretamente no seu sistema.
+	echo "[!] java.exe nao encontrado. Verifique se o Java esta instalado corretamente no seu sistema."
 	pauseProg
 	exit 1
 fi
